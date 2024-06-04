0b74f1ed6180399cb6a36ae19996757ffb509a36
Ayoyinkaaaa <Egunjobikao@gmail.com>
2024-06-04 22:38:21 +0100

    404 page

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -19,7 +19,7 @@
           <RouterLink to="/Repos">Repositories</RouterLink>
         </li>
         <li>
-          <RouterLink to="/404Error">404</RouterLink>
+          <RouterLink to="/404Error">404 pagenpm install -g serve</RouterLink>
         </li>
       </ul>
     </nav>
